2847ef08c53dbfb9e73ec77740554239b5bc4383
Ivan Shelengivskiy <vano1612shely@gmail.com>
2025-10-21 22:19:59 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { Reflector } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.enableCors();
   const reflector = app.get(Reflector);
   app.useGlobalGuards(new JwtAuthGuard(reflector));
